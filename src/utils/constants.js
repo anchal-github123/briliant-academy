cb1c8a1d071163bd8d9f0caffedaa137dc49cd4a
anchal-github123 <anchalnayak2003@gmail.com>
2025-07-14 08:29:28 +0530

    Initial commit - project setup

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const ACADEMY_NAME = "BriliantAcademy";
+
+export const ABOUT_US_TEXT = `
+BriliantAcademy is a leading coaching center dedicated to providing quality education
+to students of all levels. Our mission is to empower students with knowledge,
+confidence, and academic excellence through experienced faculty and innovative teaching methods.
+`;
+
+export const CONTACT_INFO = {
+  address: "",
+  phone: "",
+  email: "",
+};
+
+export const SOCIAL_LINKS = {
+  facebook: "",
+  instagram: "",
+  youtube: "",
+};
